5335e196389baf1c8444f0c22e44786f34d7264b
Sabbir12488 <sabbir12488@gmail.com>
2023-08-31 03:29:03 +0600

    added profile name in dashboard/index

--- admin/pages/index.tsx
+++ admin/pages/index.tsx
@@ -1,5 +1,19 @@
 import Layout from "@/components/Layout";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
-  return <Layout>test</Layout>;
+  const { data: session } = useSession();
+  return (
+    <Layout>
+      <div className="text-orange-900 flex justify-between">
+        <h2>
+          Hello, <b>{session?.user?.name}</b>
+        </h2>
+        <div className="flex bg-gray-300 gap-1 text-lg rounded-md overflow-hidden font-semibold text-black">
+          <img src={session?.user?.image ?? ""} alt="" className="w-8 h-8" />
+          <span className="px-2">{session?.user?.name}</span>
+        </div>
+      </div>
+    </Layout>
+  );
 }
