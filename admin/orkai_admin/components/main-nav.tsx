30d7130b3dcf608c3dedcc302a92d875160137b9
Sabbir12488 <sabbir12488@gmail.com>
2023-09-23 19:59:52 +0600

    something...

--- admin/orkai_admin/components/main-nav.tsx
+++ admin/orkai_admin/components/main-nav.tsx
@@ -3,7 +3,7 @@
 import { useParams, usePathname } from "next/navigation";
 
 import { cn } from "@/lib/utils";
-import Link from "next/link";
+import Link from "next/link"; // something
 
 export function MainNav({
   className,
