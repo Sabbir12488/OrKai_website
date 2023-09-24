f8b7535a5db3feb28e0ae87b27179b3411edc751
Sabbir12488 <sabbir12488@gmail.com>
2023-09-24 20:25:54 +0600

    working on billboards

--- admin/orkai_admin/app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ admin/orkai_admin/app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useParams, useRouter } from "next/navigation";
 
 import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
@@ -6,16 +7,26 @@ import { Separator } from "@/components/ui/separator";
 import { Plus } from "lucide-react";
 
 export const BillboardCilent = () => {
+  const router = useRouter();
+  const param = useParams();
+
   return (
     <>
       <div className="flex items-center justify-between">
-        <Heading title="Billboard" description="Manage billboards for your store"/>
-        <Button variant={"default"} size={"sm"}>
-          <Plus className="h-4 w-4 mr-2"/>
+        <Heading
+          title="Billboard (0)"
+          description="Manage billboards for your store"
+        />
+        <Button
+          variant={"default"}
+          size={"sm"}
+          onClick={() => router.push(`/${param.storeId}/billboards/new`)}
+        >
+          <Plus className="h-4 w-4 mr-2" />
           Add New
         </Button>
       </div>
-      <Separator/>
+      <Separator />
     </>
   );
 };
