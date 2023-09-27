a1d0dce6295102b0de8e53ddef656482219b7af8
Sabbir12488 <sabbir12488@gmail.com>
2023-09-27 18:30:43 +0600

    working on billboard

---
+++ admin/orkai_admin/app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
@@ -0,0 +1,163 @@
+"use client";
+
+import { Trash } from "lucide-react";
+import { Billboard } from "@prisma/client";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+
+import { Heading } from "@/components/ui/heading";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  Form,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import toast from "react-hot-toast";
+import axios from "axios";
+import { useParams, useRouter } from "next/navigation";
+import { AlartModel } from "@/components/models/alart-model";
+import { ApiAlart } from "@/components/ui/api-alart";
+import { useOrigine } from "@/hooks/use-origin";
+
+interface billboardFormPorps {
+  intialData: Billboard | null;
+}
+
+const formSchema = z.object({
+  label: z.string().min(1),
+  imageUrl: z.string().min(1),
+});
+
+type billboardFormValues = z.infer<typeof formSchema>; //somethings
+
+export const BillboardForm: React.FC<billboardFormPorps> = ({ intialData }) => {
+  const params = useParams();
+  const router = useRouter();
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const origin = useOrigine();
+
+  const title = intialData ? "Edit billboard" : "New billboard";
+  const description = intialData ? "Edit a billboard" : "Add a new billboard";
+  const toastMessage = intialData
+    ? "Billboard updated"
+    : "New billboard created";
+  const action = intialData ? "Save" : "Create";
+
+  const form = useForm<billboardFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: intialData || {
+      label: "",
+      imageUrl: "",
+    },
+  });
+
+  const onSubmit = async (values: billboardFormValues) => {
+    try {
+      setLoading(true);
+      await axios.patch(`/api/stores/${params.storeId}`, values);
+      router.refresh();
+      toast.success("Changes saved!");
+    } catch (error) {
+      toast.error("Something went wrong.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const onDelete = async () => {
+    try {
+      setLoading(true);
+      await axios.delete(`/api/stores/${params.storeId}`);
+      router.refresh();
+      router.push("/");
+      toast.success("Store deleted.");
+    } catch (error) {
+      toast.error("Make sure you remove all items from the store first.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <>
+      <AlartModel
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        loading={loading}
+      />
+      <div className="flex items-center justify-between">
+        <Heading title={title} description={description} />
+
+        {intialData && (
+          <Button
+            disabled={loading}
+            variant={"destructive"}
+            size={"sm"}
+            onClick={() => setOpen(true)}
+          >
+            Delete
+            <Trash className="h-4 mr-0 ml-2" />
+          </Button>
+        )}
+      </div>
+      <Separator />
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-5 w-full"
+        >
+          <FormField
+            control={form.control}
+            name="imageUrl"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Background Image</FormLabel>
+                <FormControl>
+                  <Input
+                    disabled={loading}
+                    placeholder="Billboard label"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="grid grid-cols-3 gap-8">
+            <FormField
+              control={form.control}
+              name="label"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Label</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={loading}
+                      placeholder="Billboard label"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+
+          <Button disabled={loading} className="ml-auto" type="submit">
+            {action}
+          </Button>
+        </form>
+      </Form>
+      <Separator />
+    </>
+  );
+};
--- admin/orkai_admin/app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
+++ admin/orkai_admin/app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -1,5 +1,6 @@
 import prismadb from "@/lib/prismadb";
 import React from "react";
+import { BillboardForm } from "./components/billboard-form";
 
 const BillboardPage = async ({
   params,
@@ -12,7 +13,13 @@ const BillboardPage = async ({
     },
   });
 
-  return <div>This is a form for billboards: {billboard?.label}</div>;
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <BillboardForm intialData={billboard}/>
+      </div>
+    </div>
+  );
 };
 
 export default BillboardPage; // adsd
