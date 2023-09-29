65799136b886991bd960f1c2126401b010b7262d
Sabbir12488 <sabbir12488@gmail.com>
2023-09-29 14:45:20 +0600

    working on a menu in billboard page

--- admin/orkai_admin/app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action.tsx
+++ admin/orkai_admin/app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action.tsx
@@ -1,17 +1,36 @@
-"use client"
+"use client";
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
 import { BillboardColumn } from "./columns";
+import { Edit, MoreHorizontal } from "lucide-react";
 
-interface cellActionProps{
-    data: BillboardColumn;
-};
+interface cellActionProps {
+  data: BillboardColumn;
+}
 
-export const CellAction: React.FC<cellActionProps> = ({
-    data
-}) => {
-    return (
-        <div>
-            buttons
-        </div>
-    );
+export const CellAction: React.FC<cellActionProps> = ({ data }) => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button size={"icon"} variant={"outline"} className="h-8 w-8 p-0">
+          <span className="sr-only">Menu</span>
+          <MoreHorizontal className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+        <DropdownMenuItem>
+          <Edit className="h-4 w-4 mr-2" />
+          Edit
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 };
