2a7d6413c6cf2a803245f9bb024f7e3e52c6e795
Sabbir12488 <sabbir12488@gmail.com>
2023-10-08 18:22:26 +0600

    added api to billboards

--- admin/orkai_admin/app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ admin/orkai_admin/app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -8,6 +8,7 @@ import { Plus } from "lucide-react";
 
 import { BillboardColumn, columns } from "./columns";
 import { DataTable } from "@/components/ui/data-table";
+import { ApiList } from "@/components/ui/api-list";
 
 interface billboardCilentProps {
   data: BillboardColumn[];
@@ -35,6 +36,9 @@ export const BillboardCilent: React.FC<billboardCilentProps> = ({ data }) => {
       </div>
       <Separator />
       <DataTable columns={columns} data={data} searchKey="label"/>
+      <Heading title="API" description="API calls for billboards"/>
+      <Separator/>
+      <ApiList entityName="billboards" entityIdName="billboardId"/>
     </>
   );
 };
--- admin/orkai_admin/components/ui/api-list.tsx
+++ admin/orkai_admin/components/ui/api-list.tsx
@@ -1,11 +1,51 @@
 "use client";
 
-export const ApiList = () => {
-    //something
-    //nothing
-    return (
-        <div>
-            Api List
-        </div>
-    )
+import { useOrigine } from "@/hooks/use-origin";
+
+import { useParams } from "next/navigation";
+import { ApiAlart } from "@/components/ui/api-alart";
+
+interface apiListProps {
+  entityName: string;
+  entityIdName: string;
 }
+
+export const ApiList: React.FC<apiListProps> = ({
+  entityIdName,
+  entityName,
+}) => {
+  const params = useParams();
+  const origin = useOrigine();
+
+  const baseUrl = `${origin}/api/${params.storeId}`;
+
+  return (
+    <>
+      <ApiAlart
+        title="GET"
+        variant="public"
+        description={`${baseUrl}/${entityName}`}
+      />
+      <ApiAlart
+        title="GET"
+        variant="public"
+        description={`${baseUrl}/${entityName}/{${entityIdName}}`}
+      />
+      <ApiAlart
+        title="POST"
+        variant="admin"
+        description={`${baseUrl}/${entityName}`}
+      />
+      <ApiAlart
+        title="PATCH"
+        variant="admin"
+        description={`${baseUrl}/${entityName}/{${entityIdName}}`}
+      />
+      <ApiAlart
+        title="DELETE"
+        variant="admin"
+        description={`${baseUrl}/${entityName}/{${entityIdName}}`}
+      />
+    </>
+  );
+};
